1c2e8aa4621dea4c09b860bb01f4c1cc9a6ceaa2
Josef Materi <josef.materi@gmail.com>
2022-01-20 10:59:37 +0100

    Update c4.js

--- c4.js
+++ c4.js
@@ -7,8 +7,8 @@
  * https://raw.githubusercontent.com/tobiashochguertel/draw-io/master/C4-drawIO.xml
  */
 Draw.loadPlugin(function (ui) {
-    var sidebar_id = 'c4';
-    var sidebar_title = 'C4 Notation';
+    var sidebar_id = 'c4-advanced';
+    var sidebar_title = 'C4 Advanced Notation';
 
     var c4Utils = {};
     c4Utils.isC4 = function (cell) {
